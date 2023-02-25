b70ecda6c3d3dad8b5247b0c0b20980b0ac53f50
naoki.nishio <naooo8021@icloud.com>
2023-02-25 22:11:02 +0900

    Initial Commit

---
+++ LoginTest/LoginTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginTestApp.swift
+//  LoginTest
+//
+//  Created by Nishio Naoki on 2023/02/25.
+//
+
+import SwiftUI
+
+@main
+struct LoginTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
